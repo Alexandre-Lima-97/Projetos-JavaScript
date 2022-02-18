cd3c8e0d8af2caf0917a737ab38e3bd1609640e4
Alexandre <alexandre_lima_97@hotmail.com>
2022-02-18 17:28:13 -0300

    primeiro projeto

---
+++ Timer/sources/scripts/scripts.js
@@ -0,0 +1,13 @@
+function main() {
+        const hour = document.querySelector('.hour');
+        const minutes = document.querySelector('.minutes');
+        const seconds = document.querySelector('.seconds');
+
+        hour.onchaged = function () {
+                console.log(" waweaw")
+        }
+
+        
+}
+
+main()
